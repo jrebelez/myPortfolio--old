76143b009fde9325ca5fba7b4d7cc778201cd96c
Josh Rebelez <59lpjrebelez@gmail.com>
2019-04-23 12:40:29 -0700

    navbar reactstrap

--- src/components/bodyComponent.js
+++ src/components/bodyComponent.js
@@ -13,7 +13,7 @@ class Body extends Component {
       <div id="full-screen" >
       
        <div className="container">
-       <div className="introduction">
+       <div id="into" className="introduction">
        <div className="row justify-content-center">
        
       
@@ -27,25 +27,30 @@ class Body extends Component {
               </div>
 
        </div>
-       <div className= "about">
+       <div id="aboutme" className= "about">
        <div className="container">
        
        <div className="row">
        <h1> A little about me... </h1>
+       
          
       </div>
       </div>
       </div>
       <div className="container">
-       <div className="introduction">
+       <div id="skills" className="introduction">
        <div className="row justify-content-center">
        
       
-          <h1> Hello my name is Josh </h1>
+          <h1> Here is some of my skills... </h1>
           
-          <h5> I am a full stack developer that enjoys
-              making websites that are intuitive and are well pleasing 
-              to the eye </h5>
+          <ul>
+            <li> Bootstrap </li>
+            <li> React/Redux </li>
+            <li> Angular </li>
+            <li> React Native </li>
+            <li> Node.js </li>
+            </ul>
               
               </div>
               </div>
--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import { Button, Jumbotron } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, 
+} from 'reactstrap';
 
 class Header extends Component {
   constructor(props) {
     super(props);
+
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
 
   render() {
     return (
-      <div className="header">
-        <div className="container" >
-       
-          <div className="row ">
-          
-          <i className="fa fa-bars fa-lg " aria-hidden="true"></i>
-          
-          
-          </div>
-          </div>
-          </div>
-       
-      
+<div>
+        <Navbar style={{backgroundColor: '#4a6f9a'}} light expand="md">
+          <NavbarBrand href="#intro">Josh Rebelez</NavbarBrand>
+         
+        
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="#aboutme">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#skills">Skills</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="">Contact</NavLink>
+              </NavItem>
+              
+            </Nav>
+        
+        </Navbar>
+      </div>
     );
   }
 }
